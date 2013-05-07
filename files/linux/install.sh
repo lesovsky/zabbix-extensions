1781878425005a68e114c80a66d0ae79e2c87cad
Lesovsky Alexey <lesovsky@gmail.com>
2013-05-07 12:49:18 +0600

    Linux Extended: add check open descriptors

---
+++ files/linux/install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+if [ $(id -u) -ne 0 ]; then echo "Warning: need root privileges."; exit 1; fi
+
+if [ $(which zabbix-sender) ]
+  then echo "Info: zabbix-sender found." 
+  else echo "Warning: zabbix-sender not found in PATH. Exit."; exit 1
+fi
+
+if [ ! -d /etc/zabbix/scripts ]; then
+  mkdir -p /etc/zabbix/scripts
+  cp -v ./scripts/*.sh /etc/zabbix/scripts/
+fi
+
+chown zabbix: /etc/zabbix/scripts/*.sh
+chmod 755 /etc/zabbix/scripts/*.sh
+
+echo "Info: add task into root crontab."
+(crontab -l; echo "0 */2 * * * /etc/zabbix/scripts/check-open-descriptors.sh") |uniq - |crontab -
