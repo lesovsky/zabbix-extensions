ec4b44d36ce9c676b166dfdf9c5f767a9c359a52
Lesovsky Alexey <lesovsky@gmail.com>
2013-05-17 11:04:07 +0600

    glusterfs client: config, scripts, template

---
+++ files/glusterfs-client/scripts/glusterfs.discovery.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# glusterfs mounts auto-discovery
+
+mountpoints=$(grep glusterfs /etc/fstab |grep -v ^# |awk '{print $2}')
+
+printf "{\n";
+printf "\t\"data\":[\n\n";
+
+for mount in ${mountpoints}
+do
+    printf "\t{\n";
+    printf "\t\t\"{#MOUNT}\":\"$mount\"\n";
+    printf "\t},\n";
+done
+
+printf "\n\t]\n";
+printf "}\n";
