5b1fd3111d934288b8e000bbbcd5ec1aa90f3296
Lesovsky Alexey <lesovsky@gmail.com>
2013-07-31 14:50:41 +0600

    postgresql: streaming lag now in seconds. fixes

--- files/postgresql/scripts/pgsql.streaming.lag.sh
+++ files/postgresql/scripts/pgsql.streaming.lag.sh
@@ -3,16 +3,7 @@
 # лаг потоковой репликации БД
 
 username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
-
-#если имя базы не получено от сервера, то имя берется из ~zabbix/.pgpass
-if [ -z "$*" ]; 
-  then 
-    echo "ZBX_NOTSUPPORTED. uninitialized variable master" ; exit 1;
-  else
-    master="$1"
-fi
-
-dbname=$(grep $master ~zabbix/.pgpass |cut -d: -f3)
+dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3)
 
 s=$(psql -qAtX -h localhost -U $username $dbname -c "select extract(epoch from pg_last_xact_replay_timestamp())" |cut -d. -f1)
 now=$(psql -qAtX -h localhost -U $username $dbname -c "select extract(epoch from now())" |cut -d. -f1)
