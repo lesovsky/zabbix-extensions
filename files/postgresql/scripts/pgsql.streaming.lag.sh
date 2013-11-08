1981e6ce79c00d0803b97609a10b2708532150f9
Lesovsky Alexey <lesovsky@gmail.com>
2013-11-08 14:58:16 +0500

    Update pgsql.streaming.lag.sh

--- files/postgresql/scripts/pgsql.streaming.lag.sh
+++ files/postgresql/scripts/pgsql.streaming.lag.sh
@@ -5,8 +5,6 @@
 username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
 dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3)
 
-s=$(psql -qAtX -h localhost -U $username $dbname -c "select extract(epoch from pg_last_xact_replay_timestamp())" |cut -d. -f1)
-now=$(psql -qAtX -h localhost -U $username $dbname -c "select extract(epoch from now())" |cut -d. -f1)
+r=$(psql -qAtX -h localhost -U $username $dbname -c "select round(extract(epoch from now() - pg_last_xact_replay_timestamp()))")
 
-r=$(echo $(( $now - $s )))
 if [ "0" -ge "$r" ]; then echo 0; else echo $r; fi
