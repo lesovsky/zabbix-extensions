6e703279976d05908889d90efaece31ea36d1b26
Alexey Lesovsky <lesovsky@gmail.com>
2013-12-03 16:42:20 +0600

    pgsql.bgwriter.sh fixes

--- files/postgresql/scripts/pgsql.bgwriter.sh
+++ files/postgresql/scripts/pgsql.bgwriter.sh
@@ -1,19 +1,10 @@
-#!/bin/sh
-# Author: Alexey Lesovsky
+#!/usr/bin/env bash
 # Description: bgwriter statistics. http://www.postgresql.org/docs/9.2/static/monitoring-stats.html#PG-STAT-BGWRITER-VIEW
 
+[[ -f ~zabbix/.pgpass ]] || { echo "ERROR: ~zabbix/.pgpass not found" ; exit 1; }
 username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
-
-#если имя базы не получено от сервера, то имя берется из ~zabbix/.pgpass
-if [ "$#" -lt 2 ]; 
-  then 
-    if [ ! -f ~zabbix/.pgpass ]; then echo "ERROR: ~zabbix/.pgpass not found" ; exit 1; fi
-    dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3);
-  else
-    dbname="$2"
-fi
+dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3)
 PARAM="$1"
 
 query="SELECT $PARAM FROM pg_stat_bgwriter"
-
 psql -qAtX -c "$query" -h localhost -U "$username" "$dbname" 2>/dev/null || { echo ZBX_NOTSUPPORTED; exit 1; }
