c456633ff88142eb660d9b3af4765b38347dcddf
Alexey Lesovsky <lesovsky@gmail.com>
2014-05-07 13:18:00 +0600

    swap discovery: rewrite json generation procedure

--- files/linux/scripts/swap.discovery.sh
+++ files/linux/scripts/swap.discovery.sh
@@ -6,12 +6,6 @@ SIZE=$(swapon -s |grep -v ^Filename |awk '{sum += $3} END {print sum}')
 
 if [ -z $SIZE ]; then exit 1; fi
 
-printf "{\n";
-printf "\t\"data\":[\n\n";
-
-printf "\t{\n";
-printf "\t\t\"{#SWAP_EXISTS}\":\"$line\"\n";
-printf "\t},\n";
-
-printf "\n\t]\n";
-printf "}\n";
+echo -n '{"data":['
+echo -n "{\"{#SWAP_EXISTS}\": \"$line\"}";
+echo -n ']}'
