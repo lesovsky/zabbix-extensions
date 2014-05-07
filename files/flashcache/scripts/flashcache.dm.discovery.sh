253ab466bf8e440fd452051eb17f0ce064480369
Alexey Lesovsky <lesovsky@gmail.com>
2014-05-07 13:08:29 +0600

    flashcache: rewrite json generation procedure

--- files/flashcache/scripts/flashcache.dm.discovery.sh
+++ files/flashcache/scripts/flashcache.dm.discovery.sh
@@ -4,15 +4,6 @@
 
 dms=$(for dm in $(sudo dmsetup ls |awk '{print $1}'); do if (sudo dmsetup status $dm |grep flashcache &> /dev/null); then echo $dm; fi; done)
 
-printf "{\n";
-printf "\t\"data\":[\n\n";
-
-for dm in ${dms}
-do
-    printf "\t{\n";
-    printf "\t\t\"{#DMNAME}\":\"$dm\"\n";
-    printf "\t},\n";
-done
-
-printf "\n\t]\n";
-printf "}\n";
+echo -n '{"data":['
+for dm in $dms; do echo -n "{\"{#DMNAME}\": \"$dm\"},"; done |sed -e 's:\},$:\}:'
+echo -n ']}'
--- files/flashcache/scripts/flashcache.vol.discovery.sh
+++ files/flashcache/scripts/flashcache.vol.discovery.sh
@@ -6,15 +6,6 @@ if [ ! -d /proc/flashcache ]; then exit 1; fi
 
 volumes=$(ls -1d /proc/flashcache/*/ 2> /dev/null |cut -d\/ -f4)
 
-printf "{\n";
-printf "\t\"data\":[\n\n";
-
-for vol in ${volumes}
-do
-    printf "\t{\n";
-    printf "\t\t\"{#VOLNAME}\":\"$vol\"\n";
-    printf "\t},\n";
-done
-
-printf "\n\t]\n";
-printf "}\n";
+echo -n '{"data":['
+for vol in $volumes; do echo -n "{\"{#VOLNAME}\": \"$vol\"},"; done |sed -e 's:\},$:\}:'
+echo -n ']}'
