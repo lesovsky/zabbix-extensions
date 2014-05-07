d31345fdad0b8eeb2964d331d3707fa529cb493f
Alexey Lesovsky <lesovsky@gmail.com>
2014-05-07 13:19:43 +0600

    glusterfs-client: rewrite json generation procedure

--- files/glusterfs-client/scripts/glusterfs.discovery.sh
+++ files/glusterfs-client/scripts/glusterfs.discovery.sh
@@ -4,15 +4,6 @@
 
 mountpoints=$(grep glusterfs /etc/fstab |grep -v ^# |awk '{print $2}')
 
-printf "{\n";
-printf "\t\"data\":[\n\n";
-
-for mount in ${mountpoints}
-do
-    printf "\t{\n";
-    printf "\t\t\"{#MOUNT}\":\"$mount\"\n";
-    printf "\t},\n";
-done
-
-printf "\n\t]\n";
-printf "}\n";
+echo -n '{"data":['
+for mount in $mountpoints; do echo -n "{\"{#MOUNT}\": \"$mount\"},"; done |sed -e 's:\},$:\}:'
+echo -n ']}'
