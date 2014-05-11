35d04d13c6225ef385c46e5d10ebe660828ca28b
Alexey Lesovsky <lesovsky@gmail.com>
2014-05-11 20:36:47 +0600

    remove install.sh files as unnecessary

--- files/memcached/install.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-if [ $(id -u) -ne 0 ]; then echo "Warning: need root privileges."; exit 1; fi
-
-if [ ! -d /etc/zabbix/zabbix-agentd.d ]; then
-  mkdir -p /etc/zabbix/zabbix-agentd.d
-  cp -v ./memcached.conf /etc/zabbix/zabbix-agentd.d/
-fi
-
-chown zabbix: /etc/zabbix/zabbix-agentd.d/memcached.conf
