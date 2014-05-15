51ed2a563dc4ac6e38c1bede1fcc24e61b37a195
root <root@aires.postgresql-consulting.com>
2014-05-15 13:58:59 +0400

    postgresql: add hot-standby discovery, rewriten streaming lag determining

---
+++ files/postgresql/scripts/pgsql.streaming.discovery.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Author: Alexey Lesovsky
+# Description: Low level discovery for streaming replication hot-standby servers.
+
+username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
+
+#если имя базы не получено от сервера, то имя берется из ~zabbix/.pgpass
+if [ -z "$*" ]; 
+  then 
+    if [ ! -f ~zabbix/.pgpass ]; then echo "ERROR: ~zabbix/.pgpass not found" ; exit 1; fi
+    dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3);
+  else
+    dbname="$2"
+fi
+PARAM="$1"
+
+replica_list=$(psql -qAtX -h localhost -U $username -tl --dbname=$dbname -c "SELECT client_addr FROM pg_stat_replication")
+
+echo -n '{"data":['
+for replica in $replica_list; do echo -n "{\"{#HOTSTANDBY}\": \"$replica\"},"; done |sed -e 's:\},$:\}:'
+echo -n ']}'
--- files/postgresql/scripts/pgsql.streaming.lag.sh
+++ files/postgresql/scripts/pgsql.streaming.lag.sh
@@ -1,10 +1,19 @@
-#!/bin/sh
+#!/usr/bin/env bash
 # Author: Alexey Lesovsky
-# лаг потоковой репликации БД
+# Description: Streaming replication lag (run on master).
 
 username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
-dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3)
 
-r=$(psql -qAtX -h localhost -U $username $dbname -c "select round(extract(epoch from now() - pg_last_xact_replay_timestamp()))")
+#если имя базы не получено от сервера, то имя берется из первой строки ~zabbix/.pgpass
+if [ "$#" -lt 2 ]; 
+  then 
+    if [ ! -f ~zabbix/.pgpass ]; then echo "ERROR: ~zabbix/.pgpass not found" ; exit 1; fi
+    dbname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f3);
+  else
+    dbname="$2"
+fi
 
-if [ "0" -ge "$r" ]; then echo 0; else echo $r; fi
+replica=$1
+query="select round(pg_xlog_location_diff(sent_location, replay_location) /1024 /1024,3) from pg_stat_replication where client_addr = '$replica'"
+
+psql -qAtX -h localhost -U $username $dbname -c "$query"
