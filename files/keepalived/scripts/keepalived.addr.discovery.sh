e66a4f76fdd5f2a0b04ddcf41c1417d21ab37bc4
Alexey Lesovsky <lesovsky@gmail.com>
2014-09-15 15:28:38 +0600

    keepalived.addr.discovery: remove broadcast address from ouput (when full syntax used)

--- files/keepalived/scripts/keepalived.addr.discovery.sh
+++ files/keepalived/scripts/keepalived.addr.discovery.sh
@@ -7,7 +7,7 @@ KEEPALIVED_CONF=$1
 [[ $1 ]] || { echo "ZBX_NOTSUPPORTED, need at least one parameter in which keepalived.conf must be specified"; exit 1; }
 [[ -f $KEEPALIVED_CONF ]] || { echo "ZBX_NOTSUPPORTED, $KEEPALIVED_CONF doesn't exist" ; exit 1; }
 
-ADDRESSES=$(sed -n -e '/virtual_ipaddress {/,/}/p' $KEEPALIVED_CONF |grep -v ^# |grep -oE '([0-9]{1,3}[\.]){3}[0-9]{1,3}*')
+ADDRESSES=$(sed -n -e '/virtual_ipaddress {/,/}/p' $KEEPALIVED_CONF |grep -v ^# |awk '{print $1}' |grep -oE '([0-9]{1,3}[\.]){3}[0-9]{1,3}*')
 
 echo -n '{"data":['
 for addr in $ADDRESSES; do echo -n "{\"{#KADDR}\": \"$addr\"},"; done |sed -e 's:\},$:\}:'
