c741b354f2c0c91eb5406e8d2a96980b61460160
Rodolphe Quiédeville <rodolphe@quiedeville.org>
2014-09-19 15:25:54 +0200

    Stop using /etc/pgbouncer.ini as ~zabbix/.pgpass is enough

--- files/pgbouncer/scripts/pgbouncer.pool.discovery.sh
+++ files/pgbouncer/scripts/pgbouncer.pool.discovery.sh
@@ -3,11 +3,11 @@
 # Description:	Pgbouncer pools auto-discovery
 
 if [ ! -f ~zabbix/.pgpass ]; then echo "ERROR: ~zabbix/.pgpass not found" ; exit 1; fi
-config='/etc/pgbouncer.ini'
-hostname=$(grep -w ^listen_addr $config |cut -d" " -f3 |cut -d, -f1)
-port=6432
-dbname="pgbouncer"
+
+hostname=$(head -n 1 ~zabbix/.pgpass |cut -d: -f1)
+port=$(head -n 1 ~zabbix/.pgpass |cut -d: -f2)
 username=$(head -n 1 ~zabbix/.pgpass |cut -d: -f4)
+dbname="pgbouncer"
 
 if [ '*' = "$hostname" ]; then hostname="127.0.0.1"; fi
 
