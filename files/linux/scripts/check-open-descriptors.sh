a429a41e3a73f3ded4564342de37f8330472565f
Anton Patsev <patsev.anton@gmail.com>
2018-01-11 19:04:34 +0600

    Refactored system.deleted_filehandler_maxsize in Extended monitoring for Linux

--- files/linux/scripts/check-open-descriptors.sh
+++ files/linux/scripts/check-open-descriptors.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
-# Author:	Lesovsky A.V.
+# Author:   Lesovsky A.V.
 # Description:  Search open handlers deleted files and output file size associated with the handler.
 
 LSOF=$(which lsof)
-ZBX_SERVER=$(grep -w ^Server /etc/zabbix/zabbix_agentd.conf |cut -d= -f2 |cut -d, -f1)
 GLOBAL_BIG=0
 
 for mount in $(cat /proc/mounts |grep -wE 'ext[2-4]+|xfs' |awk '{print $2}');
   do
-    LOCAL_BIG=$($LSOF $mount |grep -i del |awk '{print $7}' |sort -n |tail -n1)
+    LOCAL_BIG=$(sudo $LSOF $mount |grep -i del |awk '{print $7}' |sort -n |tail -n1)
     [[ ! -z "$LOCAL_BIG" && $LOCAL_BIG -ge $GLOBAL_BIG ]] && GLOBAL_BIG=$LOCAL_BIG
   done
 
-zabbix_sender -z $ZBX_SERVER -s $(hostname) -k system.deleted_filehandler_maxsize -o "$GLOBAL_BIG" &> /dev/null
+echo $GLOBAL_BIG
