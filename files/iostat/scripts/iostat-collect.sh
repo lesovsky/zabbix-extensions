283efa1c1a13c7751f06f5faab9fdba5da82dab4
Stephan Knauss <info@osm-tools.org>
2018-02-23 00:44:37 +0100

    iostat: use portable number format
    - use LC_ALL=C to have portable number format
    - quote variables to prevent globbing/word splitting
    
    fix #26

--- files/iostat/scripts/iostat-collect.sh
+++ files/iostat/scripts/iostat-collect.sh
@@ -7,8 +7,11 @@ SECONDS=$2
 TOFILE=$1
 IOSTAT="/usr/bin/iostat"
 
+# be portable regarding number format
+LC_ALL=C ; export LC_ALL
+
 [[ $# -lt 2 ]] && { echo "FATAL: some parameters not specified"; exit 1; }
 
-DISK=$($IOSTAT -x 1 $SECONDS | awk 'BEGIN {check=0;} {if(check==1 && $1=="avg-cpu:"){check=0}if(check==1 && $1!=""){print $0}if($1=="Device:"){check=1}}' | tr '\n' '|')
-echo $DISK | sed 's/|/\n/g' > $TOFILE
+DISK=$($IOSTAT -x 1 "$SECONDS" | awk 'BEGIN {check=0;} {if(check==1 && $1=="avg-cpu:"){check=0}if(check==1 && $1!=""){print $0}if($1=="Device:"){check=1}}' | tr '\n' '|')
+echo "$DISK" | sed 's/|/\n/g' > "$TOFILE"
 echo 0
